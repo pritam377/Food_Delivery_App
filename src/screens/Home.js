f4a9aa9505332a3c8ae6eb395031a8a5f4b6ec88
Orive2023 <67405430+pritam377@users.noreply.github.com>
2023-12-27 23:10:32 +0530

    By Pritam

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function() {
+  return (
+    <>
+            <div><Header/></div>
+            <div>Hello There</div>
+    </>
+  )
+}
